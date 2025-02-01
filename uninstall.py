50a096200a4616ce133a4c9b7133f191bc3fe249
Tyler Carney <tyler.carney@gmail.com>
2025-02-01 01:48:13 -0800

    Create uninstall.py
    stub uninstall.py

---
+++ uninstall.py
@@ -0,0 +1,4 @@
+# uninstall.py - uninstalls fxetrx and all of its dependencies and plugins from the target system
+# 
+# leave no trace of the service on the target system, and return the system to its original state
+# if there are any changes that were made to the system durring the installation process, they should be undone here
