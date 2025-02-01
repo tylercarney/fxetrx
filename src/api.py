27da6654f0093672c8c110726a0cfc01c9d4c912
Tyler Carney <tyler.carney@gmail.com>
2025-02-01 02:11:31 -0800

    Create api.py
    stub api.py and some notes/ideas... moar later

---
+++ src/api.py
@@ -0,0 +1,14 @@
+# api.py - handle the requests arriving via net.py
+# sanitize inputs
+# decide what is being asked for
+# call code
+# sanitize response
+# send response via .net
+###########################################
+# status - report if service is running, service uptime, which user it's running as, etc.
+# uptime - just get the uptime of the service (not the host)
+# version - what version of fxetrx is this?
+# misc authentication, authorization, and accounting related calls and TLS client auth
+# fetch list of plugins
+# whoami
+# ???
